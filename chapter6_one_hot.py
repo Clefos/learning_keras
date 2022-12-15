53e3fb250322bc7761a63b92d6e7484d395e5d61
Cristian Clefos <ctw00192@criticaltechworks.com>
2022-12-15 09:21:03 +0000

    imdb with pretrained networks

--- chapter6_one_hot.py
+++ chapter6_one_hot.py
@@ -1,10 +1,6 @@
 import numpy as np
 import string
 from keras.preprocessing.text import Tokenizer
-from keras.models import Sequential
-from keras.layers import Embedding, Flatten, Dense
-from keras.datasets import imdb
-from keras import preprocessing
 
 samples = ['The cat sat on the mat.', 'The dog ate my homework.']
 
@@ -58,24 +54,5 @@ for i, sample in enumerate(samples):
     for j, word in list(enumerate(sample.split()))[:max_length]:
         index = abs(hash(word)) % dimensionality
         results[i, j, index] = 1.
-print(results)
+#print(results)
 
-# Embedding
-max_features = 10000
-maxlen = 20
-
-(x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features)
-
-x_train = preprocessing.sequence.pad_sequences(x_train, maxlen=maxlen)
-x_test = preprocessing.sequence.pad_sequences(x_test, maxlen=maxlen)
-
-model = Sequential()
-model.add(Embedding(10000, 8, input_length=maxlen))
-
-model.add(Flatten())
-
-model.add(Dense(1, activation='sigmoid'))
-model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
-model.summary()
-
-history = model.fit(x_train, y_train, epochs=10, batch_size=32, validation_split=0.2)
