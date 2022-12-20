6c81bf8e5082bd9e6efbfd5cab47c1c1b8026d56
Cristian Clefos <ctw00192@criticaltechworks.com>
2022-12-20 09:18:02 +0000

    bidirectional rnns

--- chapter6_jena.py
+++ chapter6_jena.py
@@ -134,13 +134,14 @@ plt.show()
 '''
 
 model = Sequential()
-model.add(layers.GRU(32, input_shape=(None, float_data.shape[-1])))
+model.add(layers.Bidirectional(
+    layers.GRU(32, input_shape=(None, float_data.shape[-1]))))
 model.add(layers.Dense(1))
 
 model.compile(optimizer=RMSprop(), loss='mae')
 history = model.fit(train_gen,
                     steps_per_epoch=500,
-                    epochs=20,
+                    epochs=40,
                     validation_data=val_gen,
                     validation_steps=val_steps)
                     
