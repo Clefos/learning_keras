9953cd6b3084277b5f3c704ee0f9bd42fa295f90
Clefos <cristianclefos@mail.com>
2022-12-20 05:49:37 +0000

    started using gru layers

--- chapter6_jena.py
+++ chapter6_jena.py
@@ -103,7 +103,6 @@ def evaluate_naive_method():
 
 
 #evaluate_naive_method()
-
 #celsius_mae = 0.29 * std[1]
 
 model = Sequential()
@@ -111,13 +110,40 @@ model.add(layers.Flatten(input_shape=(lookback // step, float_data.shape[-1])))
 model.add(layers.Dense(32, activation='relu'))
 model.add(layers.Dense(1))
 
-model.compile(optimizer=RMSprop(), loss='mse')
+model.compile(optimizer=RMSprop(), loss='mae')
+'''
 history = model.fit(train_gen,
                     steps_per_epoch=500,
                     epochs=20,
                     validation_data=val_gen,
                     validation_steps=val_steps)
 
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(1, len(loss) + 1)
+
+plt.figure()
+
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+
+plt.show()
+'''
+
+model = Sequential()
+model.add(layers.GRU(32, input_shape=(None, float_data.shape[-1])))
+model.add(layers.Dense(1))
+
+model.compile(optimizer=RMSprop(), loss='mae')
+history = model.fit(train_gen,
+                    steps_per_epoch=500,
+                    epochs=20,
+                    validation_data=val_gen,
+                    validation_steps=val_steps)
+                    
 loss = history.history['loss']
 val_loss = history.history['val_loss']
 
