9ae7cc10ff894cc7cfc4e6f29fb92c691f3adb29
Clefos <cristianclefos@mail.com>
2023-01-10 05:03:39 +0000

    deep dream

--- chapter8_deep_dream.py
+++ chapter8_deep_dream.py
@@ -1,7 +1,11 @@
+import tensorflow as tf
+import scipy
+from keras.preprocessing import image
+import numpy as np
 from keras.applications import inception_v3
 from keras import backend as K
 
-K.set_leaning_phase(0)
+K.set_learning_phase(0)
 
 model = inception_v3.InceptionV3(weights='imagenet',
                                  include_top=False)
@@ -21,4 +25,104 @@ for layer_name in layer_contributions:
     activation = layer_dict[layer_name].output
 
     scaling = K.prod(K.cast(K.shape(activation), 'float32'))
-    loss += coeff * K.sum(K.square(activation[:, 2: -2, 2: -2, :])) / scaling
+    loss = loss + coeff * K.sum(K.square(activation[:, 2: -2, 2: -2, :])) / scaling
+
+dream = model.input
+tf.compat.v1.disable_eager_execution()
+with tf.GradientTape(persistent=True) as tape:
+    grads = tape.gradient(loss, dream)[0]
+grads /= K.maximum(K.mean(K.abs(grads)), 1e-7)
+outputs = [loss, grads]
+fetch_loss_and_grads = K.function([dream], outputs)
+
+
+def eval_loss_and_grads(x):
+    outs = fetch_loss_and_grads([x])
+    loss_value = outs[0]
+    grad_values = outs[1]
+    return loss_value, grad_values
+
+
+def gradient_ascent(x, iterations, step, max_loss=None):
+    for i in range(iterations):
+        loss_value, grad_values = eval_loss_and_grads(x)
+        if max_loss is not None and loss_value > max_loss:
+            break
+        print('...Loss value at', i, ':', loss_value)
+        x += step * grad_values
+    return x
+
+
+step = 0.01
+num_octave = 3
+octave_scale = 1.4
+iterations = 20
+max_loss = 10.
+
+
+def resize_img(img_param, size):
+    img_param = np.copy(img_param)
+    factors = (1,
+               float(size[0]) / img_param.shape[1],
+               float(size[1]) / img_param.shape[2],
+               1)
+    return scipy.ndimage.zoom(img_param, factors, order=1)
+
+
+def deprocess_image(x):
+    if K.image_data_format() == 'channels_first':
+        x = x.reshape((3, x.shape[2], x.shape[3]))
+        x = x.transpose((1, 2, 0))
+    else:
+        x = x.reshape((x.shape[1], x.shape[2], 3))
+    x /= 2.
+    x += 0.5
+    x *= 255.
+    x = np.clip(x, 0, 255).astype('uint8')
+    return x
+
+
+def save_img(img_param, fname):
+    pil_img = deprocess_image(np.copy(img_param))
+    scipy.misc.imsave(fname, pil_img)
+
+
+def preprocess_image(image_path):
+    image_loaded = image.load_img(image_path)
+    image_loaded = image.img_to_array(image_loaded)
+    image_loaded = np.expand_dims(image_loaded, axis=0)
+    image_loaded = inception_v3.preprocess_input(image_loaded)
+    return image_loaded
+
+
+base_image_path = 'cats_and_dogs_small/test/cats/cat.1500.jpg'
+img = preprocess_image(base_image_path)
+
+original_shape = img.shape[1:3]
+successive_shapes = [original_shape]
+for i in range(1, num_octave):
+    shape = tuple([int(dim / (octave_scale ** i)) for dim in original_shape])
+    successive_shapes.append(shape)
+
+successive_shapes = successive_shapes[::-1]
+
+original_img = np.copy(img)
+shrunk_original_img = resize_img(img, successive_shapes[0])
+
+for shape in successive_shapes:
+    print('Processing image shape', shape)
+    img = resize_img(img, shape)
+    img = gradient_ascent(img,
+                          iterations=iterations,
+                          step=step,
+                          max_loss=max_loss)
+    upscaled_shrunk_original_img = resize_img(shrunk_original_img, shape)
+    same_size_original = resize_img(original_img, shape)
+    lost_detail = same_size_original - upscaled_shrunk_original_img
+
+    img += lost_detail
+    shrunk_original_img = resize_img(original_img, shape)
+    save_img(img, fname='dream_at_scale_' + str(shape) + '.png')
+
+save_img(img, fname='final_dream.png')
+
